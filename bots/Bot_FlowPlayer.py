689c36b369aec4584891f7d23ebd41121b4da610
Claudio Cicali <claudio.cicali@gmail.com>
2012-03-21 23:09:59 +0100

    Reactiveted the Flowplayer bot

--- bots/Bot_FlowPlayer.py
+++ bots/Bot_FlowPlayer.py
@@ -4,10 +4,11 @@
 # -*- Mode: Python -*-
 
 from lib.AppdateHTTPRetriever import *
+from BeautifulSoup import BeautifulSoup
 
 class Bot_FlowPlayer(AppdateHTTPRetriever):
   """The FlowPlayer bot"""
-  
+
   def identify(self):
     return {
       'projectGroup': 'FlowPlayer',
@@ -15,25 +16,27 @@ class Bot_FlowPlayer(AppdateHTTPRetriever):
       'projectDescription': 'The FlowPlayer Media player',
       'projectWebSite': 'http://flowplayer.org'
     }
-  
+
   def run(self):
     self.sniffer.setBaseUrl('http://flowplayer.org')
     self.sniffer.setTargetPath('/download/index.html')
-    
+
     ##
     # Current Release
     ##
-    pattern="""Version<br/>\s*<strong>(.*?)</strong>"""
+    #print self.sniffer.getContent()
+    pattern=""""version".*>\n\s+v (.*?)\n(.*?)"""
     data = self.sniffer.setTargetPattern(pattern).run()
     if not data:
       self.say("No version retrieved")
       return KO
+
     self.set('currentVersion', data.group(1).strip())
 
     self.set('downloadUrl', "http://flowplayer.org/download/index.html")
 
     self.set('releaseNotesUrl', "http://flowplayer.org/documentation/version-history.html")
-    
+
     return OK
 
 if __name__ == "__main__":
